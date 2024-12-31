692df400760028fa41f3d6743babfa71f9311e80
Subash Pandey <subash.pandey@citytech.global>
2024-12-31 13:20:07 +0545

    Initial commit  and project setup

---
+++ src/app/pages/repo-detail/repo-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repo-detail',
+  imports: [],
+  templateUrl: './repo-detail.component.html',
+  styleUrl: './repo-detail.component.scss'
+})
+export class RepoDetailComponent {
+
+}
