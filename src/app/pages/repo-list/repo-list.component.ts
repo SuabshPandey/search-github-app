b431c3531b09722b3503a282b0517b764432f528
Subash Pandey <subash.pandey@citytech.global>
2025-01-06 16:12:06 +0545

    [Updated] Added pagintaion to the list repositories

--- src/app/pages/repo-list/repo-list.component.ts
+++ src/app/pages/repo-list/repo-list.component.ts
@@ -18,30 +18,65 @@ export class RepoListComponent implements OnInit {
   searchQuery: string = '';
   repos: any[] = [];
 
+  // Pagination related variables
+  currentPage: number = 1;
+  totalPages: number = 0;
+  totalResults: number = 0;
+  perPage: number = 10;
+
   constructor(private repoService: RepoService) { }
 
   ngOnInit() {
   }
   onSearch() {
     console.log("Search Term", this.searchQuery);
+    this.currentPage = 1;
+    this.fetchRepos();
+
+  }
+
+  fetchRepos() {
     this.isLoading = true;
-    this.repoService.getRepoList(this.searchQuery, 1, 10, 'stars', 'desc').subscribe({
+    this.repoService.getRepoList(this.searchQuery, this.currentPage, this.perPage, this.selectedSortOption, 'desc').subscribe({
       next: (res) => {
-        console.log("Response from repo api", res);
-        this.repos = res?.items
+        console.log("Response from repo api", res?.items);
+        this.repos = res?.items;
+        this.totalResults = res?.total_count;
+        this.totalPages = Math.ceil(this.totalResults / this.perPage);
         this.isLoading = false
       },
       error: (err) => {
-        console.log("Error", err);
+        this.repos = [];
+        this.totalResults = 0;
+        this.totalPages = 0;
         this.isLoading = false
-        this.repos = []
       }
     })
+  }
+
+  nextPage() {
+    console.log("Current page on next", this.currentPage);
+
+    if(this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.fetchRepos();
+    }
+  }
+
+  prevPage() {
+    console.log("Current page on prev", this.currentPage);
+    if(this.currentPage > 1) {
+      this.currentPage--;
+      this.fetchRepos();
+    }
   }
 
   onSort(event: any) {
     console.log("Sort Event", event);
     console.log("Sort Event Value", event?.target?.value);
+    this.selectedSortOption = event?.target?.value;
+    this.currentPage = 1;
+    this.fetchRepos();
 
   }
 
